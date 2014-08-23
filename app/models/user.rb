4a2758210d4aaa60340a33fef1746b6a70796fc8
samuelvary1 <sam.vary@streeteasy.com>
2014-08-23 13:00:25 -0400

    adding avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   
-  has_secure_password
+  # has_secure_password
 
   has_one :account
 
-  attr_accessible :id, :name, :email, :password, :password_confirmation
+  attr_accessible :id, :name, :email, :password, :password_confirmation, :avatar_url
+
 
   validates_uniqueness_of :email
 end
