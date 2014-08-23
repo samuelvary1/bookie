232cf6010d32329e902d7e4b9201d359b7e9c4f9
samuelvary1 <sam.vary@streeteasy.com>
2014-08-23 13:16:33 -0400

    trying to add avatar_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
+
 	def avatar_url(user)
-		if user.avatar_url.present?
-			user.avatar_url
-		else 
-			default_url = "#{root_url}images/guest.png"
+	# 	if user.avatar_url.present?
+	# 		user.avatar_url
+	# 	else
+
+			default_url = "http://latimesphoto.files.wordpress.com/2013/10/1544312_sp_1009_dodgers12_wjs.jpg"
 			gravatar_id = Digest::MD5::hexdigest(user.email).downcase
 			"http://gravatar.com/avatar/#{gravatar_id}.png?s=50"
-		end
+		# end
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   
-  # has_secure_password
+  has_secure_password
 
   has_one :account
 
-  attr_accessible :id, :name, :email, :password, :password_confirmation, :avatar_url
+  attr_accessible :id, :name, :email, :password, :password_confirmation
 
 
   validates_uniqueness_of :email
+
 end
