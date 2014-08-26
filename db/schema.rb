fe538016fcb711c6b5e9cd453b3301d28d710f43
samuelvary1 <sam.vary@streeteasy.com>
2014-08-26 16:09:43 -0400

    getting there with cancan

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140826191430) do
+ActiveRecord::Schema.define(:version => 20140826200413) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "balance",    :limit => 255
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20140826191430) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "bio"
+    t.integer  "roles_mask"
   end
 
 end
